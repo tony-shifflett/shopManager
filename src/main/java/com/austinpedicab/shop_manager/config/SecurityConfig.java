ce54d26ae9206f38c285b8d9da7d19243e04868b
Tony Shifflett <charles.l.shifflett@gmail.com>
2025-01-16 10:13:15 -0600

    added SecurityConfig

---
+++ src/main/java/com/austinpedicab/shop_manager/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.austinpedicab.shop_manager.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+
+        http.authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers("/rider/**").authenticated()
+                .anyRequest().permitAll()
+        );
+
+        http.formLogin(formLogin -> formLogin
+                .loginPage("/login/login")
+                .loginProcessingUrl("/login/loginSubmit")
+        );
+
+        http.logout(formlogout -> formlogout
+                .invalidateHttpSession(true)
+                .logoutUrl("/logout/logout")
+                .logoutSuccessUrl("/")
+                .deleteCookies("username","JSESSIONID")
+        );
+
+        http.exceptionHandling(exception -> exception
+                .accessDeniedPage("/404")
+        );
+
+        return http.build();
+    }
+
+    @Bean(name = "passwordEncoder")
+    public PasswordEncoder getPasswordEncoder() {return new BCryptPasswordEncoder();}
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+}
