2539bc760f3f9a15d628a3c75e82ce2ebc1d979f
Tony Shifflett <charles.l.shifflett@gmail.com>
2025-01-16 08:56:14 -0600

    initial commit

---
+++ src/test/java/com/austinpedicab/shop_manager/ShopManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.austinpedicab.shop_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
